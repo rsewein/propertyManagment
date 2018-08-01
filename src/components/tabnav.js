697eb69691cfed47309d9858b381832595dc64c0
Ramsey Seweingyawma <rsewein@gmail.com>
2018-08-01 10:20:29 -0600

    tabnav grid.

--- src/components/tabnav.js
+++ src/components/tabnav.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 
 class TabNav extends Component {
-render() {
-    var JSX = [
-        <h1>TABS</h1>
-    ];
+    render() {
+        return (
+            <div className="tab-nav">
 
-    this.props.tabs.map((tab, index) => {
-        JSX.push(tab.component);
-    })
-
-    return JSX;
+            </div>
+        )
     }
 }
 
