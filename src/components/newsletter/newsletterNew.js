eec98a5c1efc9f69a30a16652fe7fdcd03c991e9
Ramsey Seweingyawma <rsewein@gmail.com>
2018-08-09 17:11:21 -0600

    Finally finished newsletter new form.

--- src/components/newsletter/newsletterNew.js
+++ src/components/newsletter/newsletterNew.js
@@ -5,18 +5,16 @@ import NewNewsletterForm from './newsletterNewForm';
 class NewNewsletter extends Component {
 
     onSubmit = (fields) => {
-        console.log('trying to submit')
+        
         // if(button == 'submit') {
         //     // save new newsletter to packend. Perform a post request here.
         //     console.log('trying to submit to backend.');
         // }
-        // this.props.history.push('/dashboard')
+        this.props.history.push('/dashboard')
     }
 
     onCancel = (fields) => {
-        console.log('trying to cancel')
-
-        // this.props.history.push('/dashboard')
+        this.props.history.push('/dashboard')
     }
 
     render() {
