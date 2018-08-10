d442c61f7491c9ecdd2727211f77a2b1b5f981ca
Ramsey Seweingyawma <rsewein@gmail.com>
2018-08-10 17:09:40 -0600

    Hooked up latestnewsletter and datebox component to store to fetch data.

--- src/components/newsletter/newsletterGrid.js
+++ src/components/newsletter/newsletterGrid.js
@@ -14,25 +14,18 @@ class NewsletterGrid extends Component {
     }
 
     componentDidMount() {
-        this.props.fetchNewsletters();
+        setTimeout(() => {
+            this.props.fetchNewsletters();
+        }, 1000);
     }
 
     render() {
-
-        const latest = {
-            _id: '13',
-            title: "Happy Holidays!!!",
-            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus mi eu tellus placerat consequat. Sed maximus mi eget imperdiet cursus. Aenean a consequat libero, et ultricies augue. Aliquam rutrum dictum enim et ultricies. Vivamus posuere sem at lectus pellentesque eleifend. Fusce vitae aliquam felis, vel interdum arcu. Sed urna leo, rutrum eu fermentum ut, volutpat a purus. Maecenas elit justo, cursus at felis nec, condimentum porta odio. Donec luctus cursus turpis, eget hendrerit enim lobortis vel. Pellentesque laoreet feugiat odio in pharetra. Sed porttitor ornare velit.",
-            date: new Date(),
-            imageUrl: "http://via.placeholder.com/960x258"
-        }
-
         return (
             <div className="newsletter-grid">
                 <Button className="newsletter-grid__button" icon="fas fa-plus" callback={() => this.handleAddNewsletter()}/>
-                <NewsletterBox date={new Date()}/>
+                <NewsletterBox />
                 <NewsletterArchive/>
-                <NewsletterLatest history={this.props.history} {...latest}/>
+                <NewsletterLatest history={this.props.history} />
             </div>
         )
     }
