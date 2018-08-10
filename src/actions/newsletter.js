420dca88620496b2cf339b1f51e8cf0c7973653d
Ramsey Seweingyawma <rsewein@gmail.com>
2018-08-10 14:08:28 -0600

    Hooked up newsletterarchive to redux store mapstatetoprops to display newsletters data in the archive.

--- src/actions/newsletter.js
+++ src/actions/newsletter.js
@@ -9,14 +9,14 @@ export function fetchNewsletters() {
                 _id: '13',
                 title: "Happy Holidays!!!",
                 body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus mi eu tellus placerat consequat. Sed maximus mi eget imperdiet cursus. Aenean a consequat libero, et ultricies augue. Aliquam rutrum dictum enim et ultricies. Vivamus posuere sem at lectus pellentesque eleifend. Fusce vitae aliquam felis, vel interdum arcu. Sed urna leo, rutrum eu fermentum ut, volutpat a purus. Maecenas elit justo, cursus at felis nec, condimentum porta odio. Donec luctus cursus turpis, eget hendrerit enim lobortis vel. Pellentesque laoreet feugiat odio in pharetra. Sed porttitor ornare velit.",
-                date: new Date(),
+                date: new Date(2018, 0, 1),
                 imageUrl: "http://via.placeholder.com/960x258"
             },
             {
                 _id: '15',
                 title: "Second Newsletter!",
                 body: "Sed maximus mi eget imperdiet cursus. Aenean a consequat libero, et ultricies augue. Aliquam rutrum dictum enim et ultricies. Vivamus posuere sem at lectus pellentesque eleifend. Fusce vitae aliquam felis, vel interdum arcu. Sed urna leo, rutrum eu fermentum ut, volutpat a purus. Maecenas elit justo, cursus at felis nec, condimentum porta odio. Donec luctus cursus turpis, eget hendrerit enim lobortis vel. Pellentesque laoreet feugiat odio in pharetra. Sed porttitor ornare velit.",
-                date: new Date(),
+                date: new Date(2018, 5, 1),
                 imageUrl: "http://via.placeholder.com/960x258"
             }
         ]
